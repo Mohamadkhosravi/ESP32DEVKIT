b8e7486002376c989aa16325c85188569fe85acf
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-03-01 21:53:58 +0330

    DEV:021211:first configh for web serverdone

--- lib/myWifi/myWifi.h
+++ lib/myWifi/myWifi.h
@@ -2,26 +2,29 @@
 #ifndef _MY_WIFI_H
 #define _MY_WIFI_H
 #include <Arduino.h>
-#include<WiFi.h>
-#include<WiFiClient.h>
-#include<WebServer.h>
-#define TIME_OUT_WIFI_SETUP 1000 //ms
-//fd.wifiSetup("AndroidAP54E7","12345678");
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#define TIME_OUT_WIFI_SETUP 1000 // ms
+// fd.wifiSetup("AndroidAP54E7","12345678");
 class myWifi
 {
 private:
     /* data */
-public :
+public:
     
- bool wifiSetup(char *userName,char *pasword);
- void runWifi(void);
- 
-    
-   
+    bool wifiSetup(char *userName, char *pasword);
+    void runWifi(void);
+    void WebServerSetup(void);
+    void WebServerRun(void);
+    String SendHTML(uint8_t led1stat,uint8_t led2stat);
+
+    void handle_OnConnect();
 };
 
-
-
-
-
+// void handle_led1on();
+// void handle_led1off();
+// void handle_led2on();
+// void handle_led2off();
+// void handle_NotFound();
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <Arduino.h>
-#include<BlouetoothServer.h>
-BlouetoothServer myBLI; 
+// #include<BlouetoothServer.h>
+#include<myWifi.h>
+//BlouetoothServer myBLI; 
+myWifi wifi ;
 void setup(){
 Serial.begin(115200);
-myBLI.setup();
+// myBLI.setup();
+wifi.wifiSetup("AndroidAP54E7","12345678");
+wifi.WebServerSetup();
 
 }
 void loop() {
-   char my_massage[]="Hello word"; 
-   myBLI.sendMassage(my_massage);
- 
+   //char my_massage[]="Hello word"; 
+  // myBLI.sendMassage(my_massage);
+   wifi.runWifi();
 
 }
 
