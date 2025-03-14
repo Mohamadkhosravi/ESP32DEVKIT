4b1f264df93eabe11d07ac2fa324ebf258801c37
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2025-03-15 01:02:10 +0330

    Wifi ok

---
+++ ESP32DEVKIT/lib/BluetoothServer/BlouetoothServer.cpp
@@ -0,0 +1,78 @@
+
+#include <BlouetoothServer.h>
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+  };
+};
+void BlouetoothServer::setup()
+{
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic,change to Notify mode
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_NOTIFY);
+
+  // Create a BLE Descriptor
+
+  pDescr = new BLEDescriptor((uint16_t)0x2901);
+  pDescr->setValue("A very interesting variable");
+  pCharacteristic->addDescriptor(pDescr);
+
+  pBLE2902 = new BLE2902();
+  pBLE2902->setNotifications(true);
+  pCharacteristic->addDescriptor(pBLE2902);
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void BlouetoothServer::sendMassage(char *massage)
+{
+
+  if (deviceConnected)
+  {
+
+    std::string STRmassage = massage;
+    pCharacteristic->setValue((STRmassage));
+    pCharacteristic->notify();
+    delay(1000);
+  }
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected)
+  {
+    delay(500);                  // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected)
+  {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
+}
---
+++ ESP32DEVKIT/lib/myWifi/myWifi.h
@@ -0,0 +1,34 @@
+
+#ifndef _MY_WIFI_H
+#define _MY_WIFI_H
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+// #include <WebSockets.h>
+// #include<ArduinoJson.h>
+#define TIME_OUT_WIFI_SETUP 1000 // ms
+// fd.wifiSetup("AndroidAP54E7","12345678");
+class myWifi
+{
+private:
+const char *USERNAME="admin";
+const char *PASSWORD="admin";
+    /* data */
+public:
+    myWifi(const char *UserName,const char *Password):USERNAME(UserName),PASSWORD(Password){};
+    bool wifiSetup(void);
+    void runWifi(void);
+    void WebServerSetup(void);
+    void WebServerRun(void);
+    String SendHTML(uint8_t led1stat,uint8_t led2stat);
+    void getInfo(void);
+    void handle_OnConnect();
+};
+
+// void handle_led1on();
+// void handle_led1off();
+// void handle_led2on();
+// void handle_led2off();
+// void handle_NotFound();
+#endif
