ba3cdd65e5d2aaa18d199445e54529b89d16884c
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2025-03-19 00:09:03 +0330

    Update WiFi credentials and add HTTP client functionality

--- ESP32DEVKIT/src/main.cpp
+++ ESP32DEVKIT/src/main.cpp
@@ -1,20 +1,49 @@
 #include <Arduino.h>
 #include<myWifi.h>
+#include <WiFiClientSecure.h>
+#include<HTTPClient.h>
+
 #define LED 2
-myWifi wifi("mohamadkh","809236ML7368S");
+myWifi wifi("mohammad","79006944805L8S#");
+HTTPClient http;
+WiFiClientSecure client;
+//String serverName = "https://holidayapi.ir/gregorian/{1394}}/{10}/{10}";
+String serverName = "https://timeapi.io/api/time/current/zone?timeZone=Europe%2FAmsterdam";
 void setup() {
 Serial.begin(9600); 
 wifi.wifiSetup();
 wifi.runWifi();
+client.setInsecure(); 
 pinMode(LED,OUTPUT);
+
 }
 
 void loop() {
-  
+  http.begin(serverName.c_str());
   wifi.getInfo();
   digitalWrite(LED,HIGH);
   delay(500);
   digitalWrite(LED,LOW);
   delay(500);
+  int httpResponseCode = http.GET();
+   Serial.println(httpResponseCode);  
+   if (WiFi.status() == WL_CONNECTED) {
+    http.begin(client, serverName);
+
+    int httpResponseCode = http.GET();
+    Serial.println(httpResponseCode);
+
+    if (httpResponseCode > 0) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      Serial.print("Errore ");
+      Serial.println(http.errorToString(httpResponseCode));
+    }
+     http.end();
+  } else {
+    Serial.println("WiFi Error");
+  }
+ 
   // put your main code here, to run repeatedly:
 }
