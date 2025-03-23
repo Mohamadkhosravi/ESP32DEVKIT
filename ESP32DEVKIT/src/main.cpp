2dc5a3a1b51a45075521bff5fe2ea74115027e79
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2025-03-23 23:57:26 +0330

    post test

--- ESP32DEVKIT/src/main.cpp
+++ ESP32DEVKIT/src/main.cpp
@@ -11,7 +11,7 @@ WiFiClientSecure client;
 unsigned long   lastTime=0;
 //String serverName = "https://holidayapi.ir/gregorian/{1394}}/{10}/{10}";
 //String serverName = "https://timeapi.io/api/time/current/zone?timeZone=Europe%2FAmsterdam";
-String serverName = "https://petstore.swagger.io/pet/{petId}";
+String serverName = "https://petstore.swagger.io/pet/{0}";
 
 void setup() {
 Serial.begin(9600); 
@@ -33,7 +33,11 @@ void loop() {
       Serial.println(httpResponseCode);
 
       if (httpResponseCode > 0) {
-        http.POST(" ");
+       String sendString= "{\"id\": 10,\"category\": {\"id\": 101,\"name\": \"dog\"},\"name\": \"doggie\",\"photoUrls\": [\"string\"],\"tags\": [{\"id\": 0,\"name\": \"string\"}],\"status\": \"available\"}";
+
+
+       
+        http.POST(sendString);
         String payload = http.getString(); 
         Serial.println("Payload:");
         Serial.println(payload);
